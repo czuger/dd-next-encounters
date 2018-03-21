93ae44ac83db0964a5d571f221cb2993dcc3408c
ced <zuger.cedric@gmail.com>
2018-03-21 12:25:11 +0100

    Encounters as yaml file.

--- run/encounter.rb
+++ run/encounter.rb
@@ -1,22 +1,13 @@
-require_relative 'lib/ib/monsters_manual'
-require_relative 'lib/lair'
+require_relative '../lib/monsters_manual'
+require_relative '../lib/lair'
+require 'pp'
 
-l = Lair.new( :gobelin_basic )
+l = Lair.new( :bugbear_basic )
 l.read_manuals
 
 encounter =  l.get_encounter( 1, 3, 3, 3, 3 )
 puts encounter.to_s
 
-m = MonstersManual.new
-m.read
-#
-# p m.sources
-# p m.types
-# p m.challenges
-#
-# p m.select( sources: [ 'Basic Rules', 'Monster Manual' ], types: [ 'Undead' ] ).map{ |m| m.key }
-#
-# puts
-# m.select( sources: [ 'Basic Rules', 'Monster Manual' ], types: [ 'Undead' ], min_challenge: 1.0/8, max_challenge: 2 ).each do |m|
-#   p m
-# end
+pp Lair::CONTENT[:bugbear_basic].to_yaml
+
+p YAML.load_file( 'db/encounters/gobelins.yml' )
