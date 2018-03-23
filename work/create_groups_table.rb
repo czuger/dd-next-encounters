e023f7f48f17cfdc257ebc9eeca834f548de1e4d
ced <zuger.cedric@gmail.com>
2018-03-23 11:37:13 +0100

    Create group table.

--- work/create_groups_table.rb
+++ work/create_groups_table.rb
@@ -2,18 +2,41 @@ require 'pp'
 require 'yaml'
 
 monsters_group_data = {}
+default_group_entry = '{ groups: [], boss: false, superior_to: [] }'
 
 File.open( 'data/groups.txt', 'r' ) do |f|
   f.readlines.each do |line|
     next if line[0] == '#'
     line = line.split
     next if line.empty?
-    p line
     group = line.shift.to_sym
     line.each do |m|
-      monsters_group_data[ m.to_sym ] ||= { groups: [] }
+      monsters_group_data[ m.to_sym ] ||= eval( default_group_entry )
       monsters_group_data[ m.to_sym ][ :groups ] << group
     end
+  end
+end
+
+File.open( 'data/bosses.txt', 'r' ) do |f|
+  f.readlines.each do |line|
+    next if line[0] == '#'
+    line = line.split
+    next if line.empty?
+    line.each do |m|
+      monsters_group_data[ m.to_sym ] ||= eval( default_group_entry.clone )
+      monsters_group_data[ m.to_sym ][ :boss ] = true
+    end
+  end
+end
+
+File.open( 'data/hierarchy.txt', 'r' ) do |f|
+  f.readlines.each do |line|
+    next if line[0] == '#'
+    line = line.split()
+    next if line.empty?
+    superior, _, inferior = line
+    monsters_group_data[ superior.to_sym ] ||= eval( default_group_entry.clone )
+    monsters_group_data[ superior.to_sym ][ :superior_to ] << inferior
   end
 end
 
